099eb1168c758d148c399680feb25681ea319ca4
PauAlejaP <pa.al.pa.ag12@gmail.com>
2024-02-08 18:59:00 -0500

    Creación de navbar y footer

---
+++ miPrimerProyecto/src/Footer.jsx
@@ -0,0 +1,7 @@
+import "./Footer.css";
+
+function Footer() {
+  return <h2>Todos los derechos reservados</h2>;
+}
+
+export default Footer;
---
+++ miPrimerProyecto/src/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <ul>
+      <li>Inicio</li>
+      <li>Listas</li>
+      <li>Saludos</li>
+    </ul>
+  );
+}
+
+export default Navbar;
